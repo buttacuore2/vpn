3f4f541295d88d5df7e9432967fa260b53842b74
buttacuore2 <sasijajo2@gmail.com>
2025-07-03 19:28:54 +0100

    Update install_require.sh

--- data/install_require.sh
+++ data/install_require.sh
@@ -4,7 +4,7 @@ PORT_TCP='1194'
 PORT_UDP='2200'
 
 apt update
-apt install -y curl wget cron python-minimal libpython-stdlib
+apt install -y curl wget cron python2-minimal libpython2-stdlib
 apt install -y iptables
 apt install -y openvpn netcat neofetch vnstat
 apt install -y screen stunnel4 gnutls-bin python
